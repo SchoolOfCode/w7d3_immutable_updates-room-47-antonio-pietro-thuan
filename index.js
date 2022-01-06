3cbfb2bcca0e9c5eada05399ecb609a35d56c5b8
xXm4ntr4Xx <pietroannobil@gmail.com>
2022-01-06 12:33:20 +0000

    spread exercise

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+    
+   return  [...array,item]
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+    return  [item,...array]
+}
+
+
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+   return  [...array.slice(0,index),item,...array.slice(index)]
+}
+
+
+
 
 // Immutably replace an item at a specific position/index within an array
-export function replaceItem(array, item, index) {}
+export function replaceItem(array, item, index) {
+    return [...array.slice(0,index),item,...array.slice(3)]
+}
+
+
+
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+    return [...array.slice(0,index),...array.slice(2)]
+}
 
 //Objects:
 
@@ -22,7 +41,9 @@ export function removeItem(array, index) {}
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+    return {...object, name: newName}
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
